cb6437f250bcf3af90b020f79007b9422678e599
prakashbitra <prakashbitra@gmail.com>
2026-05-27 03:12:23 +0400

    Update screener.py

--- screener.py
+++ screener.py
@@ -38,8 +38,59 @@ def check_setup(df):
     return False, 0
 
 def run_screener():
-    india_tickers = ["RELIANCE.NS", "TCS.NS", "HDFCBANK.NS", "INFY.NS"]
-    usa_tickers = ["AAPL", "MSFT", "NVDA", "AMZN"]
+# Top 200 Indian Equities (Nifty 50 + Next 50 + Midcap 100 constituents)
+    india_tickers = [
+        "RELIANCE.NS", "TCS.NS", "HDFCBANK.NS", "ICICIBANK.NS", "BHARTIARTL.NS", "SBI.NS", "INFY.NS",
+        "LICI.NS", "ITC.NS", "HINDUNILVR.NS", "LT.NS", "BAJFINANCE.NS", "HCLTECH.NS", "MARUTI.NS",
+        "SUNPHARMA.NS", "ADANIENT.NS", "KOTAKBANK.NS", "TITAN.NS", "ONGC.NS", "TATAMOTORS.NS", "NTPC.NS",
+        "AXISBANK.NS", "DMART.NS", "ADANIGREEN.NS", "ADANIPORTS.NS", "ULTRACEMCO.NS", "ASIANPAINT.NS",
+        "COALINDIA.NS", "BAJAJFINSV.NS", "BAJAJ-AUTO.NS", "POWERGRID.NS", "NESTLEIND.NS", "WIPRO.NS",
+        "M&M.NS", "IOC.NS", "JIOFIN.NS", "HAL.NS", "DLF.NS", "ADANIPOWER.NS", "JSWSTEEL.NS", "TATASTEEL.NS",
+        "SIEMENS.NS", "IRFC.NS", "VBL.NS", "ZOMATO.NS", "PIDILITIND.NS", "GRASIM.NS", "SBILIFE.NS",
+        "BEL.NS", "LTIM.NS", "TRENT.NS", "PNB.NS", "INDIGO.NS", "BANKBARODA.NS", "HDFCLIFE.NS", "ABB.NS",
+        "BPCL.NS", "PFC.NS", "GODREJCP.NS", "TATAPOWER.NS", "HINDALCO.NS", "CHOLAFIN.NS", "BOSCHLTD.NS",
+        "TVSMOTOR.NS", "RECLTD.NS", "BRITANNIA.NS", "INDUSINDBK.NS", "HAVELLS.NS", "CIPLA.NS", "SHREECEM.NS",
+        "SRF.NS", "GAIL.NS", "TECHM.NS", "DIVISLAB.NS", "EICHERMOT.NS", "AMBUJACEM.NS", "APOLLOHOSP.NS",
+        "MAXHEALTH.NS", "CGPOWER.NS", "TORNTPHARM.NS", "COLPAL.NS", "HEROMOTOCO.NS", "LODHA.NS",
+        "CANBK.NS", "TIINDIA.NS", "AUBANK.NS", "ICICIGI.NS", "JINDALSTEL.NS", "CUMMINSIND.NS", "TATACOMM.NS",
+        "IDEA.NS", "ZEC.NS", "POLYCAB.NS", "SHRIRAMFIN.NS", "UBL.NS", "MACROTECH.NS", "DIXON.NS",
+        "PERSISTENT.NS", "MUTHOOTFIN.NS", "PETRONET.NS", "ASHOKLEY.NS", "SUNDARMFIN.NS", "MRF.NS",
+        "FEDERALBNK.NS", "PIIND.NS", "PGHH.NS", "CONCOR.NS", "VOLTAS.NS", "IDFCFIRSTB.NS", "COROMANDEL.NS",
+        "ASTRAL.NS", "BALKRISIND.NS", "M&MFIN.NS", "OBEROIRLTY.NS", "GMRINFRA.NS", "PRESTIGE.NS",
+        "TATAELXSI.NS", "NMDC.NS", "ABCAPITAL.NS", "GUJGASLTD.NS", "LUPIN.NS", "PEL.NS", "BATAINDIA.NS",
+        "MFSL.NS", "AUROPHARMA.NS", "BANDHANBNK.NS", "STARHEALTH.NS", "NHPC.NS", "SAIL.NS", "YESBANK.NS",
+        "BIOCON.NS", "ICICIPRULI.NS", "BANKINDIA.NS", "UNIONBANK.NS", "MOTHERSON.NS", "TORNTPOWER.NS",
+        "SKFINDIA.NS", "SYNGENE.NS", "FORTIS.NS", "ZEEL.NS", "GLAND.NS", "IPCALAB.NS", "IGL.NS", "LTTS.NS",
+        "COFORGE.NS", "MINDTREE.NS", "MPHASIS.NS", "DALBHARAT.NS", "RAMCOCEM.NS", "LICHSGFIN.NS",
+        "CRISIL.NS", "DELHIVERY.NS", "PAYTM.NS", "NYKAA.NS", "POLICYBZR.NS", "SONACOMS.NS", "KPITTECH.NS",
+        "TATACHEM.NS", "DEVYANI.NS", "APTUS.NS", "POONAWALLA.NS", "AARTIIND.NS", "NAVINFLUOR.NS",
+        "CLEAN.NS", "TRIDENT.NS", "WELSPUNIND.NS", "RELAXO.NS", "KPRMILL.NS", "INDIAMART.NS", "MCX.NS",
+        "IEX.NS", "CAMS.NS", "CDSL.NS", "ANGELONE.NS", "UTIAMC.NS", "NAM-INDIA.NS", "BSOFT.NS", "AFFLE.NS",
+        "HAPPSTMNDS.NS", "ROUTE.NS", "LATENTVIEW.NS", "MAPMYINDIA.NS", "MEDANTA.NS", "KIMS.NS", "RAINBOW.NS",
+        "METROPOLIS.NS", "LALPATHLAB.NS", "MANAPPURAM.NS", "IIFL.NS", "CHAMBLFERT.NS", "DEEPAKNTR.NS",
+        "TATAINVEST.NS", "JSWENERGY.NS", "SUZLON.NS", "INOXWIND.NS"
+    ]
+
+    # Top 200 US Equities (S&P 500 & Nasdaq 100 mega/large cap mix)
+    usa_tickers = [
+        "AAPL", "MSFT", "NVDA", "AMZN", "META", "GOOGL", "GOOG", "TSLA", "AVGO", "LLY", "JPM", "V", "UNH",
+        "XOM", "MA", "JNJ", "PG", "HD", "MRK", "COST", "ABBV", "CVX", "CRM", "AMD", "PEP", "KO", "WMT",
+        "BAC", "TMO", "LIN", "MCD", "ADBE", "DIS", "ACN", "CSCO", "ABT", "INTC", "INTU", "QCOM", "WFC",
+        "DHR", "CMCSA", "IBM", "AMAT", "CAT", "GE", "TXN", "NOW", "PM", "NKE", "SPGI", "AMGN", "COP",
+        "HON", "BA", "PFE", "UNP", "SYK", "ISRG", "RTX", "BKNG", "GS", "PLD", "ELV", "T", "NEE", "BLK",
+        "TJX", "MDLZ", "PGR", "VRTX", "CB", "REGN", "ADP", "LMT", "MMC", "CI", "BSY", "ADI", "MU", "CVS",
+        "GILD", "ETN", "C", "ZTS", "KLAC", "FI", "SLB", "BSX", "PANW", "CME", "SCHW", "MO", "SNPS", "CDNS",
+        "DE", "CSX", "NOC", "ITW", "EQIX", "SHW", "SO", "AON", "ICE", "EOG", "WM", "TGT", "MCK", "APH",
+        "PCAR", "ORLY", "HCA", "CL", "NXPI", "FCX", "MCO", "F", "USB", "MPC", "PXD", "EMR", "PSX", "TRV",
+        "VLO", "MAR", "PH", "ROP", "MTD", "OXY", "AIG", "DHI", "TDG", "CARR", "DUK", "STZ", "PAYX", "TT",
+        "AZO", "YUM", "ADSK", "IQV", "O", "WELL", "GPN", "EXC", "KMB", "SRE", "HLT", "AEP", "ROST", "CTAS",
+        "CPRT", "MNST", "TEL", "KMI", "D", "CHTR", "KDP", "WMB", "FAST", "PRU", "GWW", "SYY", "PEG", "AFL",
+        "SPG", "AWK", "LVS", "BIIB", "CTSH", "EA", "XEL", "VRSK", "WBA", "ILMN", "IDXX", "ALGN", "MTB",
+        "TROW", "DFS", "ALL", "ROK", "OTIS", "AMP", "CBRE", "DLTR", "SBAC", "WDC", "GLW", "AME", "FITB",
+        "YUMC", "NUE", "GPC", "VRSN", "KHC", "EPAM", "KEYS", "MTCH", "EXPD", "ODFL", "WST", "ANSS", "ZBH",
+        "CDW", "DOV", "VMC", "MLM", "XYL", "IT", "CHD", "FMC", "TSCO", "BBY", "ULTA", "VFC", "HRL", "MKC",
+        "SBUX", "SNA", "CINF", "L", "GL", "RJF", "PFG", "CMA", "ZION", "KEY", "CFG", "HBAN", "RF"
+    ]
     
     results = {"india": [], "usa": [], "last_updated": ""}
     
